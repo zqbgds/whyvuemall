f9e08ce1a1ee7c19ffee68c3407fabef1fe686cc
zaki <zqbgds@163.com>
2020-10-24 14:30:05 +0800

    封装首页导航栏

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">Vue商城</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
     export default {
-        name: "Home"
+      name: "Home",
+      components: {
+        NavBar
+      }
     }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
