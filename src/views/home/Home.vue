e87d93ebf1f89923543d146e24284eccb46161e5
zaki <zqbgds@163.com>
2020-10-27 16:44:16 +0800

    9.16 引入老师写的轮播图组件，有一个问题只有当点过第一个图片返回后才能正常显示轮播

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,21 @@
     <nav-bar class="home-nav">
       <div slot="center">Vue商城</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
-  import { getHomeMultidata } from "network/home";
+  import NavBar from "components/common/navbar/NavBar"
+  import HomeSwiper from './childComps/HomeSwiper'
+
+  import { getHomeMultidata } from "network/home"
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
     },
     data(){
       return {
@@ -24,7 +28,7 @@
     created() {
       // 1.请求多条数据
       getHomeMultidata().then(res => {
-        // console.log(res)
+        // console.log(this.banners = res.data.data.banner.list)
         this.banners = res.data.data.banner.list
         this.recommends = res.data.data.recommend.list
       })
