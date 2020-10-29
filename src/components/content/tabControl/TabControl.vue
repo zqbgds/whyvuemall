54fda9349db23efac3ab9e166e0c79db75480864
zaki <zqbgds@163.com>
2020-10-29 15:01:22 +0800

    10.2首页顶部tabControl的封装

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="tab-control">
+      <div v-for="(item,index) in titles"
+           class="tab-control-item" @click="itemClick(index)"
+           :class="{active:index === currentIndex}">
+        <span>{{item}}</span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "TabControl",
+      props: {
+        titles: {
+          type: Array,
+          default(){
+            return []
+          }
+        }
+      },
+      data(){
+        return {
+          currentIndex:0,
+          isActive:false
+        }
+      },
+      methods:{
+        itemClick(index){
+          this.currentIndex = index
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    background-color: #fff;
+  }
+  .tab-control-item{
+    flex: 1;
+    height: 40px;
+    line-height: 40px;
+    font-size: 14px;
+  }
+  .active{
+    color: var(--color-high-text);
+
+  }
+  .active span{
+    padding: 5px;
+    border-bottom: 3px solid var(--color-high-text);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,6 +6,7 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
     <ul>
       <li>列表1</li>
       <li>列表2</li>
@@ -113,6 +114,7 @@
 
 <script>
   import NavBar from "components/common/navbar/NavBar"
+  import TabControl from "components/content/tabControl/TabControl";
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from "./childComps/RecommendView";
   import FeatureView from "./childComps/FeatureView";
@@ -123,6 +125,7 @@
     name: "Home",
     components: {
       NavBar,
+      TabControl,
       HomeSwiper,
       RecommendView,
       FeatureView
@@ -156,5 +159,9 @@
     right: 0;
     top: 0;
     z-index: 6;
+  }
+  .tab-control{
+    position: sticky;
+    top:44px
   }
 </style>
