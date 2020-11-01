84b4dc35ed0702f86c2aed13d6f40c899754ee95
李强 <zqbgds@163.com>
2020-11-01 19:30:43 +0800

    10.6 TabControl点击切换商品

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,9 +6,8 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
-
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
   </div>
 </template>
 
@@ -41,7 +40,13 @@
           pop: {page: 0, list: []},
           new: {page: 0, list: []},
           sell: {page: 0, list: []}
-        }
+        },
+        currentType: 'pop'
+      }
+    },
+    computed: {
+      showGoods(){
+        return this.goods[this.currentType].list
       }
     },
     created() {
@@ -53,6 +58,25 @@
       this.getHomeGoods('sell')
     },
     methods: {
+      /**
+       * 事件监听相关方法
+       */
+      tabClick(index){
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+                break
+          case 1:
+            this.currentType = 'new'
+                break
+          case 2:
+            this.currentType = 'sell'
+        }
+        console.log(index)
+      },
+      /**
+       * 网络请求相关方法
+       */
       getHomeMultidata(){
         getHomeMultidata().then(res => {
           this.banners = res.data.data.banner.list
